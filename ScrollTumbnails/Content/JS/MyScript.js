b68176709fa556025eadb3d114d8b9d40de50050
Vlad Kasatkin <mozartdv@live.ru>
2014-05-05 20:35:09 +1100

    Навесил обработчики событий и добавил анимацию ленты.

---
+++ ScrollTumbnails/Content/JS/MyScript.js
@@ -0,0 +1,28 @@
+﻿var sum = 0;
+$(function () {
+    $("#scroll_left").on("click", click_left);
+    $("#scroll_right").on("click", click_right);
+});
+
+$(window).load(function () {
+    
+    $("a").each(function () {
+        //console.log(index + ": " + $(this).text());
+        sum = sum + $(this).width();
+    });
+
+    $("div.container_img").width(sum);
+    console.log(sum);
+
+
+    });
+
+function click_left() {
+    console.log("Нажата влево");
+    $(".container_img").animate({ "margin-left": "+=100px" }, "slow");
+};
+
+function click_right() {
+    console.log("Нажата вправо");
+    $(".container_img").animate({ "margin-left": "-=100px" }, "slow");
+};
