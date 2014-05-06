d92fe85a5f1041f1c5cfe7a67a9ffef8907e7b1b
Vlad Kasatkin <mozartdv@live.ru>
2014-05-06 22:06:04 +1100

    Добавил стоперы скролла

--- ScrollTumbnails/Content/JS/MyScript.js
+++ ScrollTumbnails/Content/JS/MyScript.js
@@ -1,7 +1,9 @@
 ﻿var sum = 0;
+var dWidth = 0;
+var dWidthTemp = 0;
 $(function () {
-    $("#scroll_left").on("click", click_left);
-    $("#scroll_right").on("click", click_right);
+    //$("#scroll_left").on("click", click_left);
+    //$("#scroll_right").on("click", click_right);
 });
 
 $(window).load(function () {
@@ -13,16 +15,44 @@ $(window).load(function () {
 
     $("div.container_img").width(sum);
     console.log(sum);
+    dWidth = sum - $("div.container_scroll").width();
+    dWidthTemp = dWidth;
+    console.log(dWidth);
 
-
+    if (dWidth>0) {
+        $("#scroll_left").on("click", click_left);
+        $("#scroll_right").on("click", click_right);
+    }
     });
 
 function click_left() {
     console.log("Нажата влево");
-    $(".container_img").animate({ "margin-left": "+=100px" }, "slow");
+    //$(".container_img").animate({ "margin-left": "+=100px" }, "slow");
+    if (dWidthTemp <= (dWidth - 100)) {
+        $(".container_img").animate({ "margin-left": "+=100px" }, "slow");
+        dWidthTemp = dWidthTemp + 100;
+    }
+    else {
+        if (dWidthTemp != dWidth) {
+            $(".container_img").animate({ "margin-left": "+=" + (dWidth - dWidthTemp) + "px" }, "slow");
+            dWidthTemp = dWidth;
+        }
+        
+    }
 };
 
 function click_right() {
     console.log("Нажата вправо");
-    $(".container_img").animate({ "margin-left": "-=100px" }, "slow");
+    if (dWidthTemp>=100) {
+        //$(".container_img").animate({ "margin-left": "-=100px" }, "slow");
+        $(".container_img").animate({ "margin-left": "-=" + 100 + "px" }, "slow");
+        dWidthTemp = dWidthTemp - 100;
+    }
+    else {
+        if (dWidthTemp>0) {
+            $(".container_img").animate({ "margin-left": "-=" + dWidthTemp + "px" }, "slow");
+            dWidthTemp = 0;
+        }
+    }
+    
 };
